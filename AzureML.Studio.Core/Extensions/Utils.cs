11139247799fe729dcae010688b9bd33286edfb0
Borys Rybak <borys.rybak@gmail.com>
2018-05-23 22:15:40 +0200

    :pencil2: Fix class names.

---
+++ AzureML.Studio.Core/Extensions/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureML.Studio.Core.Extensions
+{
+    internal static class Utils
+    {
+        internal static Task<HttpResponseMessage> PatchAsJsonAsync(this HttpClient client, string requestUri, string jsonBody)
+        {
+            var stringContent = new StringContent(jsonBody, Encoding.ASCII, "application/json");
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), requestUri) { Content = stringContent };
+
+            return client.SendAsync(request);
+        }
+    }
+}
