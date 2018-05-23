e0f2786757e7c59d1644daadac37b5f025d03050
Borys Rybak <borys.rybak@gmail.com>
2018-05-23 19:40:25 +0200

    :construction: Add extensions class.

---
+++ AzureML.Studio/Utils/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AzureML.Studio.Utils
+{
+    public static class Extensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (T element in source)
+            {
+                action(element);
+            }
+        }
+    }
+}
