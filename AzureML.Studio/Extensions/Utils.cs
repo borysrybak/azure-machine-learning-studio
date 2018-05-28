0c49c80d54863e103db2739ecb6040eae70ace2d
Borys Rybak <borys.rybak@gmail.com>
2018-05-28 13:21:06 +0200

    :construction: Add utility to extension methods.

--- AzureML.Studio/Extensions/Utils.cs
+++ AzureML.Studio/Extensions/Utils.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 
 namespace AzureML.Studio.Extensions
 {
@@ -11,6 +13,34 @@ namespace AzureML.Studio.Extensions
             {
                 action(element);
             }
+        }
+
+        public static string GetDescription<T>(this T e) where T : IConvertible
+        {
+            var description = string.Empty;
+
+            if (e is Enum)
+            {
+                var type = e.GetType();
+                var values = Enum.GetValues(type);
+
+                foreach (int val in values)
+                {
+                    if (val == e.ToInt32(CultureInfo.InvariantCulture))
+                    {
+                        var memInfo = type.GetMember(type.GetEnumName(val));
+                        var descriptionAttributes = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                        if (descriptionAttributes.Length > 0)
+                        {
+                            description = ((DescriptionAttribute)descriptionAttributes[0]).Description;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            return description;
         }
     }
 }
