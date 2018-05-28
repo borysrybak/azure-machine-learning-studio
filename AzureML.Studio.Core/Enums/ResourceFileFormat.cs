495b1a59d0fa43192b2b7d65f6379a50fc05f174
Borys Rybak <borys.rybak@gmail.com>
2018-05-28 13:21:25 +0200

    :construction: Create enum file.

---
+++ AzureML.Studio.Core/Enums/ResourceFileFormat.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace AzureML.Studio.Core.Enums
+{
+    public enum ResourceFileFormat
+    {
+        [Description("GenericCSV")]
+        GenericCSV,
+
+        [Description("GenericCSVNoHeader")]
+        GenericCSVNoHeader,
+
+        [Description("GenericTSV")]
+        GenericTSV,
+
+        [Description("GenericTSVNoHeader")]
+        GenericTSVNoHeader,
+
+        [Description("ARFF")]
+        ARFF,
+
+        [Description("Zip")]
+        Zip,
+
+        [Description("RData")]
+        RData,
+
+        [Description("PlainText")]
+        PlainText
+    }
+}
