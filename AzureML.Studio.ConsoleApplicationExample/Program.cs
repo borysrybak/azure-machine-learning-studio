08b0c28c6341c52a29560f2ea7c2a1a462fba8b0
Borys Rybak <borys.rybak@gmail.com>
2018-06-18 21:16:03 +0200

    :construction: Change code.

--- AzureML.Studio.ConsoleApplicationExample/Program.cs
+++ AzureML.Studio.ConsoleApplicationExample/Program.cs
@@ -11,18 +11,18 @@ namespace AzureML.Studio.ConsoleApplicationExample
 
             var sWS = new WorkspaceSettings()
             {
-                WorkspaceId = "ac248c970b46421f992e6b03ad6701ba",
-                AuthorizationToken = "IBDZaU/W6Xlvy9yKMifhtNOqQ/0umXZGrCVyU4L7jnxeNMZM9M1UHawQrR6+qSz7OpZyExRe7WLP0bq/3Hwdyw==",
-                Location = "West Europe"
+                WorkspaceId = "x",
+                AuthorizationToken = "y",
+                Location = "z"
             };
 
-            var experimentId = "ac248c970b46421f992e6b03ad6701ba.f-id.39b55411a6ba4b3bb9f45d100e4ce04d";
+            var experimentId = "x";
 
             var dWS = new WorkspaceSettings()
             {
-                WorkspaceId = "5378385d551e4011a444388fbf9290c4",
-                AuthorizationToken = "ulxHRR/5wVl1JZN8iADRHi0czGFzK5sV5T9JmALDcuU/WCo8NJOwA6e7bNRJyQvVm9quhIen5RdjhyxTmLx2eg==",
-                Location = "West Europe"
+                WorkspaceId = "x",
+                AuthorizationToken = "y",
+                Location = "z"
             };
 
             studioClient.CopyExperiment(sWS, experimentId, dWS);
